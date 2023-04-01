e4708738a523fac42b1b4c51b1b7eff0e8e5ca59
Debasis Dakua <102685615+Debasis-Dakua@users.noreply.github.com>
2023-04-01 12:27:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import cv2
+
+video=cv2.VideoCapture(0)
+
+
+faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+while True:
+        ret,frame=video.read()
+        faces=faceDetect.detectMultiScale(frame,1.3,5)
+        for x,y,w,h in faces:
+            x1,y1=x+w,y+h
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),1)#draw the rect
+
+            cv2.line(frame, (x, y),(x+30, y),  (255, 255, 0),  6)#top left
+            cv2.line(frame, (x, y), (x, y+30), (255, 255, 0), 6)
+
+            cv2.line(frame, (x1, y), (x1 - 30, y), (255, 255, 0), 6)  # top right
+            cv2.line(frame, (x1, y), (x1, y + 30), (255, 255, 0), 6)
+
+            cv2.line(frame, (x, y1), (x + 30, y1), (255, 255, 0), 6)  # bottom left
+            cv2.line(frame, (x, y1), (x, y1 - 30), (255, 255, 0), 6)
+
+            cv2.line(frame, (x1, y1), (x1 - 30, y1), (255, 255, 0), 6)  # bottom right
+            cv2.line(frame, (x1, y1), (x1, y1- 30), (255, 255, 0), 6)
+
+        cv2.imshow("Frame",frame)
+        k=cv2.waitKey(1)
+        if k==ord('q'):
+            break
+video.release()
+cv2.destroyAllWindows()
